e4f87dd090162bcc8f96316e39ae813eff19b2f6
matias regazzo <matiasregazzo2003@gmail.com>
2023-12-26 10:11:13 -0300

    Comentarios y limite de request eliminados

--- app.py
+++ app.py
@@ -9,19 +9,16 @@ context_manager.load_db()
 history_manager = HistoryManager()
 
 llm = LLM()
-print("Hola hola me imprimi 1")
+
 app = Flask(__name__)
-print("Hola hola me imprimi 2")
 cors = CORS(app)
-print("Hola hola me imprimi 3")
 app.config['CORS_HEADERS'] = "Content-Type"
-print("Hola hola me imprimi 4")
+
+
 @app.route('/question', methods=['POST'])
 @cross_origin()
 def question():
     data = request.json
-    if len(data["historial"]) >= 8:
-        return {"message": "Si querés seguir haciendo consultas, suscribite y usá sin límites la herramienta de IA"}
     matches = context_manager.get_matches(data["historial"][-1]["content"])
     prompt = "Contexto: " + matches + "\n\nMensaje: " + data["historial"][-1]["content"]
     response = llm.run(data["historial"], prompt)
